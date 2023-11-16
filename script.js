7b2dd21e5aeaab2a67079e7f498a8ae73835c601
Linicker <olinicker@gmail.com>
2023-11-16 15:47:23 -0300

    js - toogle dark and light mode

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function toggleMode() {
-  alert("Testando");
+  const html = document.documentElement;
+
+  if(html.classList.contains("light")){
+    html.classList.remove('light');
+  }else	{
+    html.classList.add('light');
+  }
 }
